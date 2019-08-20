16bbb422a46e46eebef00692ddedfefcab463389
Lukas Fam <takeruadelbert@gmail.com>
2019-08-20 21:37:41 +0700

    added an outline

---
+++ ImmutableQueue/src/immutablequeue/ImmutableQueue.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package immutablequeue;
+
+/**
+ *
+ * @author taker
+ */
+public final class ImmutableQueue<T> implements Queue<T> {
+
+    @Override
+    public Queue<T> enQueue(T t) {
+        return null;
+    }
+    
+    @Override
+    public Queue<T> deQueue() {
+        return null;
+    }
+    
+    @Override
+    public T head() {
+        return null;
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
